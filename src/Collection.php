d2c3ce8f3961aba53947d2728223e20ac9a2a04f
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-16 00:01:32 -0500

    refactor to return all types of collections

--- src/Collection.php
+++ src/Collection.php
@@ -6,7 +6,6 @@ use GuzzleHttp\Psr7\Uri;
 class Collection
 {
     const COLLECTIONS_ENDPOINT = 'collections';
-    const BILLS = 'BILLS';
     
     /**
      * Constructs an instance
@@ -23,7 +22,7 @@ class Collection
      * 
      * @return \stdClass
      */
-    public function get() : \stdClass
+    public function index() : \stdClass
     {
         $objUri = new Uri();
         return $this->objApi->get($objUri->withPath(self::COLLECTIONS_ENDPOINT));
@@ -31,15 +30,18 @@ class Collection
     
     /**
      * 
+     * @param string $type
+     * @param \Cnizzardini\GovInfo\Requestor\Collection $objRequestor
+     * @return \stdClass
      */
-    public function bills(\Cnizzardini\GovInfo\Requestor\BillCollection $objRequestor) : \stdClass
+    public function item(string $strCollectionCode, \Cnizzardini\GovInfo\Requestor\CollectionRequestor $objRequestor) : \stdClass
     {
         $objUri = new Uri();
         
         $objUri = $objUri->withQueryValue($objUri, 'pageSize', $objRequestor->getIntPageSize());
         $objUri = $objUri->withQueryValue($objUri, 'offset', $objRequestor->getIntOffSet());
         
-        $strPath = self::COLLECTIONS_ENDPOINT . '/' . self::BILLS;
+        $strPath = self::COLLECTIONS_ENDPOINT . '/' . $strCollectionCode;
         
         if ($objRequestor->getObjStartDate()) {
             $strPath.= '/' . urlencode($objRequestor->getObjStartDate()->format('Y-m-d H:i:s'));
