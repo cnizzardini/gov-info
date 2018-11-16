33d6a2c297e64b121c8aa625f91e6b1b24045408
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-15 22:17:27 -0500

    comments, typed arguments, and return types

--- src/Api.php
+++ src/Api.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Cnizzardini\GovInfo;
 
 class Api
@@ -7,25 +8,47 @@ class Api
     private $objHttp;
     private $strApiKey;
     
+    /**
+     * Construct an instance
+     * 
+     * @param \GuzzleHttp\Client $objHttp
+     * @param string $strApiKey
+     */
     public function __construct(\GuzzleHttp\Client $objHttp, string $strApiKey)
     {
         $this->objHttp = $objHttp;
         $this->strApiKey = $strApiKey;
     }
     
-    public function get($strEndPoint)
+    /**
+     * HTTP GET
+     * 
+     * @param string $strEndPoint
+     * @return \stdClass
+     */
+    public function get(string $strEndPoint) : \stdClass
     {
         $strUrl = ''; //self::URL . $strEndPoint . '?api_key=' . $this->strApiKey;
         $objResponse = $this->objHttp->get($strUrl);
         return json_decode($objResponse->getBody()->getContents());
     }
     
-    public function getObjHttp()
+    /**
+     * Return Guzzle Client
+     * 
+     * @return \GuzzleHttp\Client
+     */
+    public function getObjHttp() : \GuzzleHttp\Client
     {
         return $this->objHttp;
     }
     
-    public function getStrKey()
+    /**
+     * Return API Key
+     * 
+     * @return string
+     */
+    public function getStrKey() : string
     {
         return $this->strApiKey;
     }    
