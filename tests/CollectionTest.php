e96dff85b23f7a651e82218e111c3f845cbf9582
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-15 23:54:34 -0500

    added test for bills

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -5,6 +5,7 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
 use Cnizzardini\GovInfo\Api;
 use Cnizzardini\GovInfo\Collection;
+use Cnizzardini\GovInfo\Requestor\BillCollection;
 
 class CollectionTest extends TestCase
 {
@@ -26,4 +27,39 @@ class CollectionTest extends TestCase
         
         $this->assertEquals('USCOURTS', $objItem->collectionCode);
     }
+    
+    public function testBills()
+    {
+        $objMockHandler = new MockHandler();
+        $objMockHandler->append(new Response(200, [], file_get_contents(self::FIXTURES . 'collections-bills.json')));
+        $objClient = new Client([
+            'handler' => $objMockHandler
+        ]);
+        
+        $objApi = new Api($objClient, self::KEY);
+        $objCollection = new Collection($objApi);
+        
+        $objResult = $objCollection->bills(new BillCollection());
+        $objItem = reset($objResult->packages);
+        $this->assertEquals('BILLS-115hr2740rfs', $objItem->packageId);
+    }
+    
+    public function testBillsWithDates()
+    {
+        $objMockHandler = new MockHandler();
+        $objMockHandler->append(new Response(200, [], file_get_contents(self::FIXTURES . 'collections-bills.json')));
+        $objClient = new Client([
+            'handler' => $objMockHandler
+        ]);
+        
+        $objApi = new Api($objClient, self::KEY);
+        $objCollection = new Collection($objApi);
+        $objRequestor = new BillCollection();
+        $objRequestor->setObjStartDate(new \DateTime('2018-01-01 12:00:00'));
+        $objRequestor->setObjEndDate(new \DateTime('2018-02-01 12:00:00'));
+        
+        $objResult = $objCollection->bills($objRequestor);
+        $objItem = reset($objResult->packages);
+        $this->assertEquals('BILLS-115hr2740rfs', $objItem->packageId);
+    }    
 }
