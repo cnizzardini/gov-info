17339c471228ff1540bd9056b4bb9d5fc203983e
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-16 00:01:42 -0500

    updated unit tests to deal with refactoring

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -5,7 +5,7 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
 use Cnizzardini\GovInfo\Api;
 use Cnizzardini\GovInfo\Collection;
-use Cnizzardini\GovInfo\Requestor\BillCollection;
+use Cnizzardini\GovInfo\Requestor\CollectionRequestor;
 
 class CollectionTest extends TestCase
 {
@@ -22,13 +22,13 @@ class CollectionTest extends TestCase
         
         $objApi = new Api($objClient, self::KEY);
         $objCollection = new Collection($objApi);
-        $objResult = $objCollection->get();
+        $objResult = $objCollection->index();
         $objItem = reset($objResult->collections);
         
         $this->assertEquals('USCOURTS', $objItem->collectionCode);
     }
     
-    public function testBills()
+    public function testItem()
     {
         $objMockHandler = new MockHandler();
         $objMockHandler->append(new Response(200, [], file_get_contents(self::FIXTURES . 'collections-bills.json')));
@@ -39,12 +39,12 @@ class CollectionTest extends TestCase
         $objApi = new Api($objClient, self::KEY);
         $objCollection = new Collection($objApi);
         
-        $objResult = $objCollection->bills(new BillCollection());
+        $objResult = $objCollection->item('BILLS', new CollectionRequestor());
         $objItem = reset($objResult->packages);
         $this->assertEquals('BILLS-115hr2740rfs', $objItem->packageId);
     }
     
-    public function testBillsWithDates()
+    public function testItemWithDates()
     {
         $objMockHandler = new MockHandler();
         $objMockHandler->append(new Response(200, [], file_get_contents(self::FIXTURES . 'collections-bills.json')));
@@ -54,11 +54,11 @@ class CollectionTest extends TestCase
         
         $objApi = new Api($objClient, self::KEY);
         $objCollection = new Collection($objApi);
-        $objRequestor = new BillCollection();
+        $objRequestor = new CollectionRequestor();
         $objRequestor->setObjStartDate(new \DateTime('2018-01-01 12:00:00'));
         $objRequestor->setObjEndDate(new \DateTime('2018-02-01 12:00:00'));
         
-        $objResult = $objCollection->bills($objRequestor);
+        $objResult = $objCollection->item('BILLS', $objRequestor);
         $objItem = reset($objResult->packages);
         $this->assertEquals('BILLS-115hr2740rfs', $objItem->packageId);
     }    
