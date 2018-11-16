b32917bc669338b6abf050dfbc6622e0db5b7f72
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-15 23:54:17 -0500

    refactored since Api::get() requires GuzzleHttp\Psr7\Uri now. Added retrieving bills collections

--- src/Collection.php
+++ src/Collection.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace Cnizzardini\GovInfo;
+use GuzzleHttp\Psr7\Uri;
 
 class Collection
 {
     const COLLECTIONS_ENDPOINT = 'collections';
+    const BILLS = 'BILLS';
     
     /**
      * Constructs an instance
@@ -23,6 +25,30 @@ class Collection
      */
     public function get() : \stdClass
     {
-        return $this->objApi->get(self::COLLECTIONS_ENDPOINT);
-    }  
+        $objUri = new Uri();
+        return $this->objApi->get($objUri->withPath(self::COLLECTIONS_ENDPOINT));
+    }
+    
+    /**
+     * 
+     */
+    public function bills(\Cnizzardini\GovInfo\Requestor\BillCollection $objRequestor) : \stdClass
+    {
+        $objUri = new Uri();
+        
+        $objUri = $objUri->withQueryValue($objUri, 'pageSize', $objRequestor->getIntPageSize());
+        $objUri = $objUri->withQueryValue($objUri, 'offset', $objRequestor->getIntOffSet());
+        
+        $strPath = self::COLLECTIONS_ENDPOINT . '/' . self::BILLS;
+        
+        if ($objRequestor->getObjStartDate()) {
+            $strPath.= '/' . urlencode($objRequestor->getObjStartDate()->format('Y-m-d H:i:s'));
+        }
+        
+        if ($objRequestor->getObjEndDate()) {
+            $strPath.= '/' . urlencode($objRequestor->getObjEndDate()->format('Y-m-d H:i:s'));
+        }
+        
+        return $this->objApi->get($objUri->withPath($strPath));
+    }
 }
