9f66dd466b01d1c7d2e4cc8724df4f05463a399b
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-15 22:18:18 -0500

    comments and return types

--- src/Collection.php
+++ src/Collection.php
@@ -1,16 +1,27 @@
 <?php
+
 namespace Cnizzardini\GovInfo;
 
 class Collection
 {
     const COLLECTIONS_ENDPOINT = 'collections';
     
+    /**
+     * Constructs an instance
+     * 
+     * @param \Cnizzardini\GovInfo\Api $objApi
+     */
     public function __construct(\Cnizzardini\GovInfo\Api $objApi)
     {
         $this->objApi = $objApi;
     }
     
-    public function get()
+    /**
+     * Returns collections available
+     * 
+     * @return \stdClass
+     */
+    public function get() : \stdClass
     {
         return $this->objApi->get(self::COLLECTIONS_ENDPOINT);
     }  
