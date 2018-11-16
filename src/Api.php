ea4ad02f5252a5722c46e3e543c794944c5ca734
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-15 22:12:34 -0500

    refactored to accept GuzzleHttp as constructor argument

--- src/Api.php
+++ src/Api.php
@@ -5,19 +5,28 @@ class Api
 {
     const URL = 'https://api.govinfo.gov/';
     private $objHttp;
+    private $strApiKey;
     
-    public function __construct($objHttp)
+    public function __construct(\GuzzleHttp\Client $objHttp, string $strApiKey)
     {
         $this->objHttp = $objHttp;
+        $this->strApiKey = $strApiKey;
     }
     
     public function get($strEndPoint)
     {
-        //$this->objHttp->get
+        $strUrl = ''; //self::URL . $strEndPoint . '?api_key=' . $this->strApiKey;
+        $objResponse = $this->objHttp->get($strUrl);
+        return json_decode($objResponse->getBody()->getContents());
     }
     
     public function getObjHttp()
     {
         return $this->objHttp;
     }
+    
+    public function getStrKey()
+    {
+        return $this->strApiKey;
+    }    
 }
