5945f9c6902df0a899cd6c5b978d6db2257d59c7
Chris Nizzardini <cnizzardini@gmail.com>
2018-11-17 15:35:03 -0500

    Package class

---
+++ src/Package.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Cnizzardini\GovInfo;
+use GuzzleHttp\Psr7\Uri;
+
+class Package
+{
+    const ENDPOINT = 'packages';
+    
+    /**
+     * Constructs an instance
+     * 
+     * @param \Cnizzardini\GovInfo\Api $objApi
+     */
+    public function __construct(\Cnizzardini\GovInfo\Api $objApi)
+    {
+        $this->objApi = $objApi;
+    }
+    
+    /**
+     * Gets package summary
+     * 
+     * @param \Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor
+     * @return \stdClass
+     * @throws \LogicException
+     */
+    public function summary(\Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor) : \stdClass
+    {
+        if (empty($objRequestor->getStrPackageId())) {
+            throw new \LogicException('PackageRequestor::strCollectionCode is required');
+        }
+        
+        $objUri = new Uri();
+        $objUri = $objUri->withQueryValue($objUri, 'pageSize', $objRequestor->getIntPageSize());
+        $objUri = $objUri->withQueryValue($objUri, 'offset', $objRequestor->getIntOffSet());
+        
+        $strPath = self::ENDPOINT . '/' . $objRequestor->getStrPackageId() . '/summary';
+        
+        $objResult = $this->objApi->getObject($objUri->withPath($strPath));
+        
+        return $objResult;
+    }
+    
+    /**
+     * Gets a package of a specified content type
+     * 
+     * @param \Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor
+     * @return \GuzzleHttp\Psr7\Response
+     * @throws \LogicException
+     */
+    public function contentType(\Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor) : \GuzzleHttp\Psr7\Response
+    {
+        if (empty($objRequestor->getStrPackageId())) {
+            throw new \LogicException('PackageRequestor::strCollectionCode is required');
+        }
+        
+        if (empty($objRequestor->getStrContentType())) {
+            throw new \LogicException('PackageRequestor::getStrContentType is required');
+        }
+        
+        $objUri = new Uri();
+        
+        $strPath = self::ENDPOINT . '/' . $objRequestor->getStrPackageId() . '/';
+        $strPath.= $objRequestor->getStrContentType();
+        
+        return $this->objApi->get($objUri->withPath($strPath));
+    }
+    
+    /**
+     * Gets a packages granules
+     * 
+     * @param \Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor
+     * @return \stdClass
+     * @throws \LogicException
+     */
+    public function granules(\Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor) : \stdClass
+    {
+        if (empty($objRequestor->getStrPackageId())) {
+            throw new \LogicException('PackageRequestor::strCollectionCode is required');
+        }
+        
+        $objUri = new Uri();
+        $objUri = $objUri->withQueryValue($objUri, 'pageSize', $objRequestor->getIntPageSize());
+        $objUri = $objUri->withQueryValue($objUri, 'offset', $objRequestor->getIntOffSet());
+        
+        $strPath = self::ENDPOINT . '/' . $objRequestor->getStrPackageId() . '/granules';
+        
+        $objResult = $this->objApi->getObject($objUri->withPath($strPath));
+        
+        return $objResult;
+    }
+    
+    public function granuleSummary(\Cnizzardini\GovInfo\Requestor\PackageRequestor $objRequestor) : \stdClass
+    {
+        if (empty($objRequestor->getStrPackageId())) {
+            throw new \LogicException('PackageRequestor::strCollectionCode is required');
+        }
+        
+        if (empty($objRequestor->getStrGranuleId())) {
+            throw new \LogicException('PackageRequestor::strGranuleId is required');
+        }
+        
+        $objUri = new Uri();
+        $objUri = $objUri->withQueryValue($objUri, 'pageSize', $objRequestor->getIntPageSize());
+        $objUri = $objUri->withQueryValue($objUri, 'offset', $objRequestor->getIntOffSet());
+        
+        $strPath = self::ENDPOINT . '/' . $objRequestor->getStrPackageId() . '/granules/';
+        $strPath.= $objRequestor->getStrGranuleId() . '/summary';
+        
+        $objResult = $this->objApi->getObject($objUri->withPath($strPath));
+        
+        return $objResult;
+    }
+}
