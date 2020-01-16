ef5dd351b69d8bd887e8e4a68f1c3b423b128d37
chris cnizzardini <cnizzardini@gmail.com>
2020-01-15 20:29:09 -0500

    Removed copy-pasta

--- src/Console/TraitApiKey.php
+++ src/Console/TraitApiKey.php
@@ -2,18 +2,8 @@
 
 namespace Cnizzardini\GovInfo\Console;
 
-use GuzzleHttp\Client;
-use Cnizzardini\GovInfo\Api;
-use Cnizzardini\GovInfo\Collection;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Output\OutputInterface;
 
-/**
- * Console application for pulling collections
- */
 trait TraitApiKey
 {
     use TraitApiKey;
