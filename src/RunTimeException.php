f0e4b8dde6e148798bb399cdb5710c199b291f4c
chris cnizzardini <cnizzardini@gmail.com>
2020-01-15 20:45:34 -0500

    Added RunTimeException

---
+++ src/RunTimeException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cnizzardini\GovInfo;
+
+use \Exception;
+
+class RunTimeException extends Exception
+{
+    // Redefine the exception so message isn't optional
+    public function __construct($message, $code = 500, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
