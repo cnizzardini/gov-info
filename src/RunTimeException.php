c071a35a24178740d636c73c34df1dd22d18ce86
chris cnizzardini <cnizzardini@gmail.com>
2020-01-19 13:51:56 -0500

    Opening brace should be on a new line

--- src/RunTimeException.php
+++ src/RunTimeException.php
@@ -7,7 +7,8 @@ use \Exception;
 class RunTimeException extends Exception
 {
     // Redefine the exception so message isn't optional
-    public function __construct($message, $code = 500, Exception $previous = null) {
+    public function __construct($message, $code = 500, Exception $previous = null)
+    {
         parent::__construct($message, $code, $previous);
     }
 }
